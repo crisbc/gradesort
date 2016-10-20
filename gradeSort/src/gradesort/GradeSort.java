1ef69dcba5a5fc0256a97e2ea3e2d22a7a2d0fd1
cristina <cristinabustamante@aol.com>
2016-10-20 00:18:46 -0600

    assignment3

--- gradeSort/src/gradesort/GradeSort.java
+++ gradeSort/src/gradesort/GradeSort.java
@@ -10,6 +10,7 @@ import java.util.Iterator;
 import java.util.Map;
 import java.util.TreeMap;
 import java.util.Set;
+import static jdk.nashorn.internal.parser.TokenType.EOL;
 /**
  *
  * @author Cristina
@@ -18,7 +19,7 @@ public class GradeSort {
 
     
     public static void main(String[] args) {
-         Map<String, Integer> studentGrades = new HashMap<String, Integer>();
+         Map<String, Integer> studentGrades = new HashMap<>();
         
         studentGrades.put("sally", 97);
         studentGrades.put("becky", 60);
@@ -29,7 +30,45 @@ public class GradeSort {
         println("Normal list");
         System.out.println(studentGrades);
         
-        //check if students are in the list
+        //test expected names
+        Map<String, Integer> expected = new HashMap<>();
+        
+        expected.put("becky", 60);
+        expected.put("bob", 80);
+        expected.put("marc", 100);
+        expected.put("mary", 80);
+         expected.put("sally", 97);
+        
+        
+        //1. Test if equal but ignore order
+        assert (studentGrades.equals(expected));
+         boolean studentinlist5 = studentGrades.equals(expected);
+        println("Are they the same names :" +studentinlist5);
+        
+        // Test to find if grade is in map
+        boolean studentinlist6 = studentGrades.containsValue(60);
+        println("Does it contain grade 60 :" +studentinlist6);
+        
+         boolean studentinlist7 = studentGrades.containsValue(70);
+        println("Does it contain grade 70 :" +studentinlist7);
+        
+        /* was trying to use this but gave me an error
+        try(Close out = outExpect("studentGrades[bob]=80",EOL,
+                                    "studentGrades[marc]=100",EOL,
+                                    "studentGrades[mary]=80",EOL,
+                                    "studentGrades[sally=97",EOL,
+                                    "studentGrades[becky]=60",EOL )){
+                    studentGrades.keySet().stream().forEach((key)-> {
+            studentGrades.put(key,studentGrades.get(key));
+        });
+        
+        for (String key : studentGrades.keySet()){
+            println("studentGrades["+key+"]=" + studentGrades.get(key));
+        }
+                }*/
+        
+        
+        //test if students are in the list
         boolean studentinlist = studentGrades.containsKey("sally");
         println("Is Sally in the list? :" +studentinlist);
         
@@ -40,14 +79,16 @@ public class GradeSort {
         println(" Is tom in the list? :" +studentinlist3);
         
         
+        //test if contains a certain name
         assert (studentGrades.containsKey("sally")) == true;
         assert (studentGrades.containsKey("becky")) == true;
         assert (studentGrades.containsKey("brenda")) == false;
+        assert (studentGrades.containsKey("ryan")) == true;
         
        
         //size of the class
        println("The size of the class is :" + studentGrades.size());
-       
+  
        //sorted by name
        println("Here is the sorted list by names");
         Map<String, Integer> sortStudents = new TreeMap<String, Integer>(studentGrades);
@@ -60,16 +101,19 @@ public class GradeSort {
             
          }
          
-         
+         //test if sorted
+        boolean studentinlist8 = sortStudents.equals(expected);
+        println("Are they sorted the same :" +studentinlist8);
+        
        
          
-         // is becky in the list
+          //test if student is still in list after removing
          assert (studentGrades.containsKey("becky")) == true;
          studentGrades.remove("becky");
          boolean studentinlist4 = studentGrades.containsKey("becky");
         println(" Is becky still in the list? :" +studentinlist4);
         assert (studentGrades.containsKey("becky")) == false;
-    }
-    
-    
+        
+    } 
+
 }
